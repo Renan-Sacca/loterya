e37489ee5ff379f224fa70a8924dc73c54fa2fea
ik <henrique@mambowifi.com>
2020-10-18 18:31:53 -0300

    paypal add

--- Home/urls.py
+++ Home/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from django.conf.urls import url, include
 
 from . import views
 
+
+
 urlpatterns = [
     path('', views.index, name='index'),
+]
 
+urlpatterns += [
+    path('testePagamento', views.view_that_asks_for_money),
+    path('paypal', include('paypal.standard.ipn.urls')),
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,12 +1,16 @@
+from django.urls import reverse
+from django.shortcuts import render
+from paypal.standard.forms import PayPalPaymentsForm
+
 from django.shortcuts import render
 from .models import fichas
+
+
+
 def index(request):
     
-    ficha = fichas.objects.all()
-    
-    dados={
-        'fichas' : ficha
-    }
+    ficha = fichas.objects.all()    
+    dados={'fichas': ficha}
 
     if request.user.is_authenticated:
         return render(request,'dashboard.html',dados)
@@ -14,5 +18,23 @@ def index(request):
         return render(request,'index.html', dados)
 
 
-    
 
+def view_that_asks_for_money(request):
+
+    # What you want the button to do.
+    paypal_dict = {
+        "business": "receiver_email@example.com",
+        "amount": "10000000.00",
+        "item_name": "name of the item",
+        "invoice": "unique-invoice-id",
+        "notify_url": "urlTeste", #request.build_absolute_uri(reverse('paypal-ipn')),
+        "return": "urlTeste", #request.build_absolute_uri(reverse('your-return-view')),
+        "cancel_return": "urlTeste", #request.build_absolute_uri(reverse('your-cancel-view')),
+        "custom": "premium_plan",  # Custom command to correlate to some function later (optional)
+    }
+
+    # Create the instance.
+    form = PayPalPaymentsForm(initial=paypal_dict)
+    context = {"form": form}
+    print(form)
+    return render(request, "paypal.html", context)
